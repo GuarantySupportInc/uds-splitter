70199e4b6f6ccc53e8d5dfa144ad83ca48d40ae2
LukeB99 <lbrown@ncigf.org>
2024-07-18 18:12:39 -0400

    progress and styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('node:path');
 
+let mainWindow;
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -49,13 +51,27 @@ app.on('window-all-closed', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+
+//main function on submission
 ipcMain.on('submission-form', (event, formData) => {
   console.log('Form data received:', formData);
   let linesPerFile = formData['lines-per-file'];
   let outputDir = formData['output-folder'];
   if (formData.filePath) {
     console.log('File path received:', formData.filePath);
+    const progressWindow = new BrowserWindow({
+      width: 400,
+      height: 250,
+      parent: mainWindow,
+      modal: true,
+      webPreferences: {
+        contextIsolation: true,
+        enableRemoteModule: false,
+      },
+    });
 
+    progressWindow.loadFile(path.join(__dirname, 'progress.html'));
     const fs = require('fs');
     fs.readFile(formData.filePath, 'utf-8', (err, data) => {
       if (err) {
@@ -63,19 +79,24 @@ ipcMain.on('submission-form', (event, formData) => {
         event.sender.send('form-submitted', 'Error processing file.');
       } 
       else {
-        console.log('File content:', data);
+        const lines = data.split('\n');
+        const numberOfLinesInFile = lines.length;
+
+
+        // progressWindow.webContents.send('progress-update', progress); needs to be within a for loop of some sort.. needs to know total lines ahead of time
         event.sender.send('form-submitted', 'Form data and file processed successfully!');
       }
     });
+    // progressWindow.webContents.send('progress-done', 'Form data and file processed successfully!');  #when done
   } 
   else {
     event.sender.send('form-submitted', 'Form data processed successfully without file.');
   }
 });
 
-
+//choose file
 ipcMain.on('open-file-dialog', (event) => {
-  const result = dialog.showOpenDialogSync({
+  const result = dialog.showOpenDialogSync(mainWindow,{
       properties: ['openFile'],
       filters: [
         { name: 'Text Files', extensions: ['txt', 'text'] },
@@ -85,6 +106,19 @@ ipcMain.on('open-file-dialog', (event) => {
   if (result && result.length > 0) {
       event.sender.send('selected-file', result[0]);
   } else {
-      event.sender.send('selected-file', 'File selection was canceled.');
+      event.sender.send('selected-file', 'canceled');
+  }
+});
+
+//choose output dir
+ipcMain.on('open-directory-dialog', (event) => {
+  const result = dialog.showOpenDialogSync(mainWindow, {
+      properties: ['openDirectory'],
+  });
+
+  if (result && result.length > 0) {
+      event.sender.send('selected-directory', result[0]);
+  } else {
+      event.sender.send('selected-directory', 'canceled');
   }
 });
