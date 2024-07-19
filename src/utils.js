b010655814eff7e473cb02369e31099203b5f43d
LukeB99 <lbrown@ncigf.org>
2024-07-19 16:30:14 -0400

    skeleton in place

---
+++ src/utils.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+function createNewTrailer(chunk, filePath, trailer) {
+  const fileName = path.basename(filePath);
+  const recordType = fileName[5];
+
+  switch (recordType) {
+    case 'A':
+    // Add your logic for type A here
+    break;
+    case 'B':
+      // Add your logic for type B here
+      break;
+    case 'F':
+      //has been validated that it is working as expected
+      const chunkLength = chunk.length;
+      const newTrailerVal = chunkLength.toString().padStart(9, '0');
+      const newTrailer = trailer.substring(0,64) + newTrailerVal + trailer.substring(73);
+      break;
+    case 'G':
+      // Add your logic for type G here
+      break;
+    case 'I':
+      // Add your logic for type I here
+      break;
+    default:
+      console.log('Invalid Record Type');
+      break;
+  }
+
+    return newTrailer;
+}
+  
+module.exports = createNewTrailer;
