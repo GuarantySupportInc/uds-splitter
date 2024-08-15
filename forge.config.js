f4b5389f69a7595a5f12e99e606dede7580696ea
Nathaniel Jennings <njennings@ncigf.org>
2024-08-14 23:22:41 -0400

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -54,7 +54,6 @@ module.exports = {
   publishers: [
     {
       name: "@electron-forge/publisher-github",
-      platforms: ['win32'],
       config: {
         repository: {
           owner: "GuarantySupportInc",
