d0a2cc5f31387f5d03f31d35bb6aee34b13dce60
Nathaniel Jennings <njennings@ncigf.org>
2024-08-14 22:08:54 -0400

    Constraining build to simplify publishing

--- forge.config.js
+++ forge.config.js
@@ -54,6 +54,7 @@ module.exports = {
   publishers: [
     {
       name: "@electron-forge/publisher-github",
+      platforms: ['win32'],
       config: {
         repository: {
           owner: "GuarantySupportInc",
